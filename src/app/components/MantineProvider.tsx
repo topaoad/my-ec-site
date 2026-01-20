1f1c624867f63b72a8aeaac8fcff1f33a9acd9c5
toproad <tattun0817@gmail.com>
2026-01-21 07:44:54 +0900

    MantineProviderWrapperにforceColorSchemeプロパティを追加し、テーマ設定のコメントを削除

--- src/app/components/MantineProvider.tsx
+++ src/app/components/MantineProvider.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { createTheme, MantineProvider } from "@mantine/core";
+import { MantineProvider } from "@mantine/core";
 import { ReactNode } from "react";
 
-// const theme = createTheme({
-//   /** Put your mantine theme override here */
-// });
-
-// theme = { theme }
-
 const MantineProviderWrapper = ({ children }: { children: ReactNode }) => {
-  return <MantineProvider>{children}</MantineProvider>;
+  return (
+    <MantineProvider forceColorScheme="light">
+      {children}
+    </MantineProvider>
+  );
 };
 
 export default MantineProviderWrapper;
